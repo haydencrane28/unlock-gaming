015c550c83d36fb9b032c990667aac3c017422e6
Hayden Crane <haydenmcrane28@gmail.com>
2026-05-23 18:30:50 -0400

    Added ability to request from GitHub

---
+++ src/checks/github.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize};
+use chrono::{DateTime, Local};
+
+#[derive(Deserialize)]
+struct GitHubEvent {
+    #[serde(rename="type")]
+    event_type: String,
+    created_at: String,
+}
+
+pub async fn request_from_github(username: &str, token: &str) -> bool {
+    let url = format!("https://api.github.com/users/{username}/events");
+
+    let dt = Local::now().format("%Y-%m-%d");
+
+    let client = reqwest::Client::new();    
+    let response = client.get(url).header("Authorization", format!("token {}", token).header("User-Agent", "unlock-gaming").send().await;
+
+    match response {
+        Err(e) => {
+            println!("Failed to get a response.")
+            return false;
+        }
+        Ok(value) => {
+            let result: Vec<GitHubEvent> = value.json().await.unwrap_or_default();
+            result.iter().any(event.event_type == "PushEvent" && event.created_at.starts_with(&dt.to_string()))
+        }
+    }
+
+
+}
